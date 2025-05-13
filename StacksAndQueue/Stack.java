12167eab42d1f6ba84e44ba70840fe70ef2bf464
Abishekjha5678 <abishekjha5678@gmail.com>
2025-05-13 17:59:05 +0530

    LeetCode-2104 Sum of Subarray Ranges

--- StacksAndQueue/Stack.java
+++ StacksAndQueue/Stack.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Deque;
 
 public class Stack {
-    
+
     // Function to find the index of the Next Smaller Element to the Left (NSL) for each element
     int[] nextSmallerToLeft(int[] nums, int n) {
         Deque<Integer> st = new ArrayDeque<>();
@@ -63,4 +63,64 @@ public class Stack {
 
         return (int) res;
     }
+
+    // LeetCode  2104 Sum of Subarray Ranges
+   /*  Approaches : 
+        STEP 1: Next Smaller to Left (NSL)
+        STEP 2: Next Smaller to Right (NSR)
+        STEP 3: Next Greater to Left (NGL)
+        STEP 4: Next Greater to Right (NGR)
+        STEP 5: Main method to compute subArrayRanges
+
+   */
+        int[] nextGreaterToLeft(int []nums, int n){
+            Deque<Integer> st = new ArrayDeque<>();
+            int result[]=new int[n];
+            Arrays.fill(result,-1);
+            for(int i=0;i<n;i++){
+                while(!st.isEmpty() && nums[st.peek()] <= nums[i] )st.pop();
+                if(!st.isEmpty())result[i]=st.peek();
+                st.push(i);
+            }
+            return result;
+        }
+        int[] nextGreaterToRight(int []nums, int n){
+            Deque<Integer> st = new ArrayDeque<>();
+            int result[]=new int[n];
+            Arrays.fill(result,n);
+            for(int i=n-1;i>=0;i--){
+                while(!st.isEmpty() && nums[st.peek()] < nums[i])st.pop();
+                if(!st.isEmpty())result[i]=st.peek();
+                st.push(i);
+            }
+            return result;
+        }
+        public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+
+        // Preprocessing steps using Monotonic Stack techniques
+        int NSL[] = nextSmallerToLeft(nums, n);
+        int NSR[] = nextSmallerToRight(nums, n);
+        int NGR[] = nextGreaterToRight(nums, n);
+        int NGL[] = nextGreaterToLeft(nums, n);
+
+        long minSum = 0, maxSum = 0;
+
+        // For each element nums[i], compute:
+        for (int i = 0; i < n; i++) {
+            int leftSmaller = i - NSL[i];   // Number of elements to the left where nums[i] is min
+            int rightSmaller = NSR[i] - i;  // Number of elements to the right where nums[i] is min
+            long minCount = (long) leftSmaller * rightSmaller;
+            minSum += minCount * nums[i];
+
+            int leftGreater = i - NGL[i];   // Number of elements to the left where nums[i] is max
+            int rightGreater = NGR[i] - i;  // Number of elements to the right where nums[i] is max
+            long maxCount = (long) leftGreater * rightGreater;
+            maxSum += maxCount * nums[i];
+        }
+
+        // Return the total difference
+        return maxSum - minSum;
+    }
+    
 }
